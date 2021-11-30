1c91a1a38883fe5559c87ded643b222b45dbb90b
Artur Drobinskiy <artur.drobinskiy@mcc-tomsk.de>
2021-11-30 19:45:34 +0700

    rework env. variables replacement in SPA

--- frontend/src/application/constants/env-variables.ts
+++ frontend/src/application/constants/env-variables.ts
@@ -1,7 +1,2 @@
-import { getEnvironmentVariableValue } from 'helpers/environment-helper';
-
-export const appVersion = () =>
-  getEnvironmentVariableValue('$REACT_APP_VERSION');
-
-export const sentryDsn = () =>
-  getEnvironmentVariableValue('$REACT_APP_SENTRY_DSN');
+export const appVersion = () => process.env.REACT_APP_VERSION;
+export const sentryDsn = () => process.env.REACT_APP_SENTRY_DSN;
--- webapi/src/MccSoft.TemplateApp.App/inject-environment-variables-to-spa.sh
+++ webapi/src/MccSoft.TemplateApp.App/inject-environment-variables-to-spa.sh
@@ -1,13 +1,18 @@
-#!/bin/sh
+#!/bin/bash
 
 # Script injects environment variables that starts with `REACT_APP_*` in minified/uglified/bundled SPA files
-# (it literally performs search and replace).
+# (it literally performs search and replace of REACT_APP_BLABLABLA:"ENV VARIABLE VALUE IS PUT HERE").
 # The technique is taken from https://github.com/zzswang/docker-nginx-react
 
-# find all env start with REACT_APP_
-SUBS=$(echo $(env | cut -d= -f1 | grep "^REACT_APP_" | sed -e 's/^/\$/'))
+set -x
 
-# replace above envs
+# find all env start with REACT_APP_
+export SUBS=$(echo $(env | cut -d= -f1 | grep "^REACT_APP_"))
 echo "inject environments ..."
-echo $SUBS
-for f in `find /webapi/wwwroot -regex ".*\.\(js\|css\|html\|json\|map\)"`; do envsubst "$SUBS" < $f > $f.tmp; mv $f.tmp $f; done
+
+for VARIABLE in $SUBS; do
+  VALUE="${!VARIABLE}"
+  for f in `find /webapi/wwwroot -regex ".*\.\(js\|css\|html\|json\|map\)"`; do
+    sed -i "s~$VARIABLE:\"[^\"]*\"~$VARIABLE:\"$VALUE\"~g" $f;
+  done
+done
