d972074aa0c0779deb1a51a6d0664d30dff20158
Artur Drobinskiy <artur.drobinskiy@gmail.com>
2021-12-09 23:35:48 +0700

    add comments for Loggly

--- webapi/Lib/Logging/MccSoft.Logging/RemoteLoggerOptions.cs
+++ webapi/Lib/Logging/MccSoft.Logging/RemoteLoggerOptions.cs
@@ -2,9 +2,24 @@ namespace MccSoft.Logging
 {
     public class RemoteLoggerOptions
     {
+        /// <summary>
+        /// Loggly token
+        /// </summary>
         public string Token { get; set; }
+
+        /// <summary>
+        /// Loggly server (usually logs-01.loggly.com)
+        /// </summary>
         public string Server { get; set; }
+
+        /// <summary>
+        /// Loggly port (usually 443)
+        /// </summary>
         public int Port { get; set; }
+
+        /// <summary>
+        /// Instance Name (usually something like 'dev/prod') that will be shown in logs as `InstanceName` field
+        /// </summary>
         public string InstanceName { get; set; }
     }
 }
