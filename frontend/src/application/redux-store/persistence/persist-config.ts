243fbf1ab2ad25853cb595c4fe0367c37af93a9b
Artur Drobinskiy <artur.drobinskiy@gmail.com>
2021-12-18 14:40:17 +0700

    strongly type redux-persistor whitelist

--- frontend/src/application/redux-store/persistence/persist-config.ts
+++ frontend/src/application/redux-store/persistence/persist-config.ts
@@ -2,11 +2,12 @@ import { PersistConfig } from 'redux-persist/es/types';
 import { createMigrate } from 'redux-persist';
 import { migrations } from './migrations';
 import storage from 'redux-persist/lib/storage';
+import { GlobalState } from '../types';
 
 export const persistConfig: PersistConfig<any> = {
   key: 'root',
   version: 0,
   storage: storage,
-  whitelist: ['auth'],
+  whitelist: ['auth'] as (keyof GlobalState)[],
   migrate: createMigrate(migrations, { debug: false }),
 };
