9a961ec155004d95cfb86cc7c24a8786514c28d4
Artur Drobinskiy <artur.drobinskiy@mcc-tomsk.de>
2022-03-11 14:06:12 +0700

    Nullable DateOnly/TimeOnly handling

--- webapi/Lib/LowLevelPrimitives/MccSoft.LowLevelPrimitives/Serialization/DateOnlyConverters/DateOnlyNewtonsoftConverter.cs
+++ webapi/Lib/LowLevelPrimitives/MccSoft.LowLevelPrimitives/Serialization/DateOnlyConverters/DateOnlyNewtonsoftConverter.cs
@@ -4,48 +4,54 @@ using Newtonsoft.Json;
 
 namespace MccSoft.LowLevelPrimitives.Serialization.DateOnlyConverters;
 
-public class DateOnlyNewtonsoftConverter : JsonConverter<DateOnly>
+public class DateOnlyNewtonsoftConverter : Newtonsoft.Json.JsonConverter<DateOnly?>
 {
     private const string DateFormat = "yyyy-MM-dd";
 
-    public override DateOnly ReadJson(
+    public override DateOnly? ReadJson(
         JsonReader reader,
         Type objectType,
-        DateOnly existingValue,
+        DateOnly? existingValue,
         bool hasExistingValue,
         JsonSerializer serializer
     )
     {
         var value = reader.Value;
+        if (value == null)
+            return null;
+
         if (value is DateTime dateTime)
             return new DateOnly(dateTime.Date.Year, dateTime.Date.Month, dateTime.Date.Day);
 
-        return DateOnly.ParseExact((string)reader.Value, DateFormat, CultureInfo.InvariantCulture);
+        return DateOnly.ParseExact((string)value, DateFormat, CultureInfo.InvariantCulture);
     }
 
-    public override void WriteJson(JsonWriter writer, DateOnly value, JsonSerializer serializer)
+    public override void WriteJson(JsonWriter writer, DateOnly? value, JsonSerializer serializer)
     {
-        writer.WriteValue(value.ToString(DateFormat, CultureInfo.InvariantCulture));
+        writer.WriteValue(value?.ToString(DateFormat, CultureInfo.InvariantCulture));
     }
 }
 
-public class TimeOnlyNewtonsoftConverter : JsonConverter<TimeOnly>
+public class TimeOnlyNewtonsoftConverter : Newtonsoft.Json.JsonConverter<TimeOnly?>
 {
     private const string TimeFormat = "HH:mm:ss.FFFFFFF";
 
-    public override TimeOnly ReadJson(
+    public override TimeOnly? ReadJson(
         JsonReader reader,
         Type objectType,
-        TimeOnly existingValue,
+        TimeOnly? existingValue,
         bool hasExistingValue,
         JsonSerializer serializer
     )
     {
-        return TimeOnly.ParseExact((string)reader.Value, TimeFormat, CultureInfo.InvariantCulture);
+        var value = reader.Value;
+        if (value == null)
+            return null;
+        return TimeOnly.ParseExact((string)value, TimeFormat, CultureInfo.InvariantCulture);
     }
 
-    public override void WriteJson(JsonWriter writer, TimeOnly value, JsonSerializer serializer)
+    public override void WriteJson(JsonWriter writer, TimeOnly? value, JsonSerializer serializer)
     {
-        writer.WriteValue(value.ToString(TimeFormat, CultureInfo.InvariantCulture));
+        writer.WriteValue(value?.ToString(TimeFormat, CultureInfo.InvariantCulture));
     }
 }
