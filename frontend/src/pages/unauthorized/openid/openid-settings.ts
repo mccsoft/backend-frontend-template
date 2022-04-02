92bd8ad7c791a464d3673431bcd039293536cd8c
Artur Drobinskiy <artur.drobinskiy@mcc-tomsk.de>
2022-04-03 00:05:27 +0700

    adjust redirect uri

--- frontend/src/pages/unauthorized/openid/openid-settings.ts
+++ frontend/src/pages/unauthorized/openid/openid-settings.ts
@@ -1,5 +1,7 @@
 export const authCallbackPath = '/auth/openid-callback';
 export const scopes = 'offline_access';
-export const backendUri = `${window.location.protocol}//${window.location.hostname}:${window.location.port}`;
+export const backendUri = `${window.location.protocol}//${
+  window.location.hostname
+}${window.location.port ? `:${window.location.port}` : ''}`;
 export const redirectUri = `${backendUri}${authCallbackPath}`;
 export const clientId = 'web_client';
