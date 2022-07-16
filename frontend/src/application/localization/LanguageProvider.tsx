4ef5ec67ad19d22873ca5e61b0166bc195287d37
Artur Drobinskiy <artur.drobinskiy@mcc-tomsk.de>
2022-07-16 15:18:40 +0700

    Upgrade to React 18 #22

--- frontend/src/application/localization/LanguageProvider.tsx
+++ frontend/src/application/localization/LanguageProvider.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { initializeLocalization } from './localization';
 
-export const LanguageProvider: React.FC = (props) => {
+export const LanguageProvider: React.FC<React.PropsWithChildren> = (props) => {
   const [isLoading, setLoading] = useState<boolean>(true);
 
   useEffect(() => {
