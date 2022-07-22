8f5ab4adee6a3f161954980fabd4ee1f525febb5
Artur Drobinskiy <artur.drobinskiy@mcc-tomsk.de>
2022-07-22 19:59:16 +0700

    cleanup

--- webapi/Lib/WebApi/MccSoft.WebApi/Patching/PatchRequestContractResolver.cs
+++ webapi/Lib/WebApi/MccSoft.WebApi/Patching/PatchRequestContractResolver.cs
@@ -3,30 +3,29 @@ using MccSoft.WebApi.Patching.Models;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Serialization;
 
-namespace MccSoft.WebApi.Patching
+namespace MccSoft.WebApi.Patching;
+
+/// <summary>
+/// Class that plugs in to Newtonsoft deserialization pipeline for classes descending from <see cref="PatchRequest{TDomain}"/>.
+/// For all properties, that are present in JSON it calls <see cref="PatchRequest.SetHasProperty"/>.`
+/// </summary>
+public class PatchRequestContractResolver : DefaultContractResolver
 {
-    /// <summary>
-    /// Class that plugs in to Newtonsoft deserialization pipeline for classes descending from <see cref="PatchRequest{TDomain}"/>.
-    /// For all properties, that are present in JSON it calls <see cref="PatchRequest.SetHasProperty"/>.`
-    /// </summary>
-    public class PatchRequestContractResolver : DefaultContractResolver
+    protected override JsonProperty CreateProperty(
+        MemberInfo member,
+        MemberSerialization memberSerialization
+    )
     {
-        protected override JsonProperty CreateProperty(
-            MemberInfo member,
-            MemberSerialization memberSerialization
-        )
+        var prop = base.CreateProperty(member, memberSerialization);
+
+        prop.SetIsSpecified += (o, o1) =>
         {
-            var prop = base.CreateProperty(member, memberSerialization);
-
-            prop.SetIsSpecified += (o, o1) =>
+            if (o is IPatchRequest patchRequest)
             {
-                if (o is IPatchRequest patchRequest)
-                {
-                    patchRequest.SetHasProperty(prop.PropertyName);
-                }
-            };
+                patchRequest.SetHasProperty(prop.PropertyName);
+            }
+        };
 
-            return prop;
-        }
+        return prop;
     }
 }
