d47c6030db5a8bd11663d7b5a6bb74bf4a9322f9
Artur Drobinskiy <artur.drobinskiy@mcc-tomsk.de>
2022-07-28 01:15:40 +0700

    Add infrastructure to set up query filters and save changes interceptor

---
+++ webapi/Lib/PersistenceHelpers/MccSoft.PersistenceHelpers/DbContextQueryFilterExtensions.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Query;
+
+namespace MccSoft.PersistenceHelpers;
+
+/// <summary>
+/// Helper class for setting up global EF Query Filter, e.g. to only show entities that belong to the user.
+/// </summary>
+public static class DbContextQueryFilterExtensions
+{
+    /// <summary>
+    /// Sets up a query filter. <typeparam name="T"></typeparam> is a base type for all Entities (e.g. TenantId or OrganisationId).
+    /// Normally every Entity that should be filtered by TenantId/OrganisationId should have a base class/interface
+    /// with TenantId property, that you could filter on.
+    /// </summary>
+    public static void SetupQueryFilter<T>(
+        this ModelBuilder builder,
+        Expression<Func<T, bool>> filter,
+        List<Type> typesToExclude = null
+    )
+    {
+        typesToExclude ??= new List<Type>();
+
+        builder.Model
+            .GetEntityTypes()
+            .Where(
+                entityType =>
+                    typeof(T).IsAssignableFrom(entityType.ClrType)
+                    && !typesToExclude.Contains(entityType.ClrType)
+                    && entityType.BaseType == null
+            )
+            .ToList()
+            .ForEach(entityType =>
+            {
+                builder
+                    .Entity(entityType.ClrType)
+                    .HasQueryFilter(ConvertFilterExpression(filter, entityType.ClrType));
+            });
+    }
+
+    private static LambdaExpression ConvertFilterExpression<TInterface>(
+        Expression<Func<TInterface, bool>> filterExpression,
+        Type entityType
+    )
+    {
+        var newParam = Expression.Parameter(entityType);
+        var newBody = ReplacingExpressionVisitor.Replace(
+            filterExpression.Parameters.Single(),
+            newParam,
+            filterExpression.Body
+        );
+
+        return Expression.Lambda(newBody, newParam);
+    }
+}
---
+++ webapi/Lib/PersistenceHelpers/MccSoft.PersistenceHelpers/PostProcessEntitiesOnSaveInterceptor.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+
+namespace MccSoft.PersistenceHelpers;
+
+/// <summary>
+/// Interceptor that allows to execute arbitrary code when entities of certain types are saved.
+/// Helpful to set something like OrganisationId/TenantId for all created/modified entities
+/// to be equal to id of current user.
+/// </summary>
+public class PostProcessEntitiesOnSaveInterceptor<TEntity, TDbContext> : SaveChangesInterceptor
+    where TDbContext : DbContext
+{
+    private readonly Action<TEntity, TDbContext> _postProcessAction;
+
+    public PostProcessEntitiesOnSaveInterceptor(Action<TEntity, TDbContext> postProcessAction)
+    {
+        _postProcessAction = postProcessAction;
+    }
+
+    public override ValueTask<InterceptionResult<int>> SavingChangesAsync(
+        DbContextEventData eventData,
+        InterceptionResult<int> result,
+        CancellationToken cancellationToken = new CancellationToken()
+    )
+    {
+        return new ValueTask<InterceptionResult<int>>(SavingChanges(eventData, result));
+    }
+
+    public override InterceptionResult<int> SavingChanges(
+        DbContextEventData eventData,
+        InterceptionResult<int> result
+    )
+    {
+        var context = eventData.Context;
+        var entitiesToPostProcess = context.ChangeTracker
+            .Entries()
+            .Where(x => x.State == EntityState.Added || x.State == EntityState.Modified)
+            .Select(x => x.Entity)
+            .OfType<TEntity>()
+            .ToList();
+
+        if (entitiesToPostProcess.Any())
+        {
+            foreach (var entity in entitiesToPostProcess)
+            {
+                _postProcessAction.Invoke(entity, (TDbContext)context);
+            }
+        }
+
+        return base.SavingChanges(eventData, result);
+    }
+}
