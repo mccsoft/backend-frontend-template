6c8836af0af91a4bd7eb8f10638e1626ea4a2dee
sergey.bulavskiy <sergey.bulavskiy@mcc-tomsk.de>
2022-08-24 16:03:18 +0700

    Fix broken App project file and adjust mssql docker container startup script.

--- scripts/mssql/start_mssql.ps1
+++ scripts/mssql/start_mssql.ps1
@@ -1,3 +1,3 @@
 docker volume create --name mssql-volume
 docker container rm mssql_db
-docker run --restart=always -e "ACCEPT_EULA=Y" -e "SA_PASSWORD=yourStrong(!)Password" -p 1433:1433 --name mssql_db --hostname mssql_db -d mcr.microsoft.com/mssql/server:2022-latest 
+docker run --restart=always -e "ACCEPT_EULA=Y" -e "MSSQL_SA_PASSWORD=yourStrong(!)Password" -p 1433:1433 --name mssql_db --hostname mssql_db -d mcr.microsoft.com/mssql/server:2022-latest 
