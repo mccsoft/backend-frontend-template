822d5a9a6522c7cdd8ed26eb656dc7b38036bd41
Artur Drobinskiy <artur.drobinskiy@mcc-tomsk.de>
2022-08-26 16:05:25 +0700

    FRONT: fix loading env variables from .env.development

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import react from '@vitejs/plugin-react';
 import svgrPlugin from 'vite-plugin-svgr';
@@ -10,63 +10,72 @@ var proxyTarget = process.env.BACKEND_URI ?? 'https://localhost:5001';
 var frontendPort = process.env.PORT ?? 5003;
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    mkcert(),
-    react(),
-    tsconfigPaths(),
-    svgrPlugin(),
-    ImportMetaEnvPlugin.vite({
-      example: '.env',
-    }),
-    visualizer(),
-  ],
-  define: {
-    'build.REACT_APP_VERSION': `"${process.env.npm_package_version}"`,
-  },
-  server: {
-    port: frontendPort as number,
-    https: true,
-    proxy: {
-      '/api': {
-        target: proxyTarget,
-        secure: false,
-        changeOrigin: true,
-      },
-      '/connect': {
-        target: proxyTarget,
-        secure: false,
-        changeOrigin: true,
-      },
-      '/swagger': {
-        target: proxyTarget,
-        secure: false,
-        changeOrigin: true,
-      },
-      '/Identity': {
-        target: proxyTarget,
-        secure: false,
-        changeOrigin: true,
-      },
-      '/.well-known': {
-        target: proxyTarget,
-        secure: false,
-        changeOrigin: true,
-      },
-      '/css': {
-        target: proxyTarget,
-        secure: false,
-        changeOrigin: true,
+export default defineConfig(({ command, mode }) => {
+  // this loads environment variables from `.env.development`
+  const env = loadEnv(mode, process.cwd(), '');
+  process.env = {
+    ...process.env,
+    ...env,
+  };
+
+  return {
+    plugins: [
+      mkcert(),
+      react(),
+      tsconfigPaths(),
+      svgrPlugin(),
+      ImportMetaEnvPlugin.vite({
+        example: '.env',
+      }),
+      visualizer(),
+    ],
+    define: {
+      'build.REACT_APP_VERSION': `"${process.env.npm_package_version}"`,
+    },
+    server: {
+      port: frontendPort as number,
+      https: true,
+      proxy: {
+        '/api': {
+          target: proxyTarget,
+          secure: false,
+          changeOrigin: true,
+        },
+        '/connect': {
+          target: proxyTarget,
+          secure: false,
+          changeOrigin: true,
+        },
+        '/swagger': {
+          target: proxyTarget,
+          secure: false,
+          changeOrigin: true,
+        },
+        '/Identity': {
+          target: proxyTarget,
+          secure: false,
+          changeOrigin: true,
+        },
+        '/.well-known': {
+          target: proxyTarget,
+          secure: false,
+          changeOrigin: true,
+        },
+        '/css': {
+          target: proxyTarget,
+          secure: false,
+          changeOrigin: true,
+        },
       },
     },
-  },
-  css: {
-    modules: {
-      localsConvention: 'camelCase',
+    css: {
+      modules: {
+        localsConvention: 'camelCase',
+      },
     },
-  },
-  build: {
-    outDir: 'build',
-    sourcemap: true,
-  },
+    build: {
+      outDir: 'build',
+      sourcemap: true,
+    },
+  };
 });
