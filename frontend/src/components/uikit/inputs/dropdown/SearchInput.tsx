5700946eb61b579392f96a9bd7dc7c3b400709f7
Artur Drobinskiy <artur.drobinskiy@mcc-tomsk.de>
2022-09-11 01:50:02 +0700

    add icons to SearchInput

--- frontend/src/components/uikit/inputs/dropdown/SearchInput.tsx
+++ frontend/src/components/uikit/inputs/dropdown/SearchInput.tsx
@@ -1,15 +1,43 @@
 import { Input, Props } from '../Input';
 import { ReactComponent as SearchIcon } from 'assets/icons/search.svg';
-import React from 'react';
+import { ReactComponent as CrossIcon } from 'assets/icons/close-button.svg';
+import React, { useCallback } from 'react';
+
+import styles from '../Input.module.scss';
+import { mergeRefs } from 'react-merge-refs';
 
 export const SearchInput: React.FC<Props> = React.forwardRef<
   HTMLInputElement,
   Props
 >(function SearchInput(props, ref) {
+  const localRef = React.useRef<HTMLInputElement>();
+  const onCrossIconClick = useCallback(() => {
+    localRef.current!.value = '';
+    props.onChange?.({
+      target: localRef.current!,
+      currentTarget: localRef.current!,
+      type: 'change',
+      preventDefault() {},
+      stopPropagation() {},
+      isDefaultPrevented(): boolean {
+        return false;
+      },
+    } as any);
+  }, []);
   return (
     <Input
-      ref={ref}
-      endAdornment={<SearchIcon />}
+      ref={mergeRefs([ref, localRef])}
+      endAdornment={
+        <>
+          {!!props.value && (
+            <CrossIcon
+              className={styles.searchInputCross}
+              onClick={onCrossIconClick}
+            />
+          )}
+          <SearchIcon />
+        </>
+      }
       variant={'normal'}
       {...props}
     />
