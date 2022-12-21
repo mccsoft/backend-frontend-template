3441c8ad0764cf986b83ff37e5c09e6f5dcaf5c6
Artur Drobinskiy <artur.drobinskiy@mcc-tomsk.de>
2022-12-22 00:10:34 +0700

    try @vitejs/plugin-react-swc

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig, loadEnv } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import react from '@vitejs/plugin-react';
+import reactSwc from '@vitejs/plugin-react-swc';
 import svgrPlugin from 'vite-plugin-svgr';
 import ImportMetaEnvPlugin from '@import-meta-env/unplugin';
 import { visualizer } from 'rollup-plugin-visualizer';
@@ -21,7 +22,8 @@ export default defineConfig(({ command, mode }) => {
   return {
     plugins: [
       mkcert(),
-      react(),
+      // react(),
+      reactSwc(),
       tsconfigPaths(),
       svgrPlugin(),
       ImportMetaEnvPlugin.vite({
