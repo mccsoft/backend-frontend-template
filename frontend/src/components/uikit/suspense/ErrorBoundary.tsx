689b45273713d15f86d3b15b0b0bcecf50f261d5
Artur Drobinskiy <artur.drobinskiy@mcc-tomsk.de>
2022-12-28 09:44:31 +0700

    handle CHUNK_LOAD error

--- frontend/src/components/uikit/suspense/ErrorBoundary.tsx
+++ frontend/src/components/uikit/suspense/ErrorBoundary.tsx
@@ -1,4 +1,7 @@
-import { convertToErrorString } from 'helpers/form/useErrorHandler';
+import {
+  convertToErrorString,
+  NetworkError,
+} from 'helpers/form/useErrorHandler';
 import { useTranslation } from 'react-i18next';
 import { useEffect, useState } from 'react';
 import { useLocation } from 'react-router';
@@ -6,6 +9,7 @@ import { Button } from '../buttons/Button';
 import React from 'react';
 
 import styles from './Loading.module.scss';
+import { QueryFactory } from 'services/api';
 
 type ErrorBoundaryFallbackProps = {
   error: Error;
@@ -26,19 +30,43 @@ export const ErrorBoundaryFallback = (props: ErrorBoundaryFallbackProps) => {
     }
   }, [initialLocation, location]);
 
+  const errorString = convertToErrorString(props.error);
+  const versionQuery = QueryFactory.VersionQuery.useVersionQuery();
+  const isServerAvailable = !!versionQuery.data;
+
+  // if error is NetworkError, but server is available, there's a high chance we are trying to request an old chunk of JS or CSS
+  // (unfortunately it's impossible to get the error code of Network response and compare it with 404).
+  // so we show proper error message and offer to reload the whole page
+  const isServerUpdated = errorString === NetworkError && isServerAvailable;
+
   return (
     <div className={styles.flexContainer}>
       <div className={styles.flexLoadingData} data-test-id="loading-error">
         <div className={styles.loading}>
           <h1>{convertToErrorString(props.error)}</h1>
-          <div>
-            <Button
-              onClick={async () => {
-                props.resetError();
-              }}
-              title={i18n.t('suspense.reload')}
-            />
-          </div>
+          <h1>{errorString}</h1>
+          {isServerUpdated ? (
+            <>
+              <div className={styles.serverUpdated}>
+                {i18n.t('suspense.server_updated')}
+              </div>
+              <Button
+                onClick={async () => {
+                  document.location.reload();
+                }}
+                title={i18n.t('suspense.reload')}
+              />
+            </>
+          ) : (
+            <div>
+              <Button
+                onClick={async () => {
+                  props.resetError();
+                }}
+                title={i18n.t('suspense.reload')}
+              />
+            </div>
+          )}
         </div>
       </div>
     </div>
--- frontend/src/helpers/form/useErrorHandler.ts
+++ frontend/src/helpers/form/useErrorHandler.ts
@@ -4,6 +4,8 @@ import { UseFormSetError } from 'react-hook-form/dist/types/form';
 import { DeepPartial } from 'react-hook-form/dist/types/utils';
 import { NavigateFunction, useNavigate } from 'react-router';
 
+export const NetworkError = 'Network Error';
+
 export type UseSendFormReturn<T> = {
   /*
   Function to be passed to form.handleSubmit
@@ -193,23 +195,28 @@ function convertToErrorStringInternal(error: any): string {
   }
 
   if (error.code === 'CSS_CHUNK_LOAD_FAILED') {
-    return 'Network Error';
+    return NetworkError;
   }
   if (error.name === 'ChunkLoadError') {
-    return 'Network Error';
+    return NetworkError;
+  }
+  if (error.message?.includes('fetch dynamically imported module')) {
+    return NetworkError;
   }
   if (error.message?.includes("Cannot read property 'status' of undefined")) {
     // nswag generated client throws it when there's no response
-    return 'Network Error';
+    return NetworkError;
   }
   if (error.message) {
     // e.g. Network Error
+    console.log('Error:', error, JSON.stringify(error));
     return error.message;
   } else if (error.title) {
     return error.title;
   } else if (error instanceof String || typeof error === 'string') {
     return error as string;
   }
+  console.log('Unknown Error:', error, JSON.stringify(error));
   return error.toString();
 }
 
