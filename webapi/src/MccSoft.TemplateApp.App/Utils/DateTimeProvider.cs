990878bffdf0d45dd16f0df80941a20fdd727783
Dmitry Zhavoronkov <rudomitori@gmail.com>
2023-04-27 12:35:57 +0700

    feat: Adjust DateTimeProvider

--- webapi/src/MccSoft.TemplateApp.App/Utils/DateTimeProvider.cs
+++ webapi/src/MccSoft.TemplateApp.App/Utils/DateTimeProvider.cs
@@ -1,16 +1,76 @@
-﻿using System;
+﻿using MccSoft.LowLevelPrimitives.Extensions;
 
 namespace MccSoft.TemplateApp.App.Utils;
 
 /// <summary>
 /// Provides a consistent value of "now" throughout a service operation.
 /// </summary>
-public class DateTimeProvider : IDateTimeProvider
+public sealed class DateTimeProvider : IDateTimeProvider
 {
-    public DateTime UtcNow { get; } = DateTime.UtcNow;
+    /// <inheritdoc />
+    public DateTimeOffset UtcNowOffset { get; } = DateTimeOffset.UtcNow;
 }
 
 public interface IDateTimeProvider
 {
-    public DateTime UtcNow { get; }
+    /// <summary>
+    /// Current date and time by UTC on server.
+    /// </summary>
+    public DateTimeOffset UtcNowOffset { get; }
+
+    /// <summary>
+    /// Current date and time by UTC on server.
+    /// </summary>
+    public DateTime UtcNow => UtcNowOffset.UtcDateTime;
+
+    /// <summary>
+    /// Current date by UTC on server
+    /// </summary>
+    public DateOnly UtcToday => UtcNow.ToDateOnly();
+}
+
+/// <summary>
+/// Implementation of IDateTimeProvider for testing purpose.
+/// Prefer this class over using <c>Mock&lt;IDateTimeProvider&gt;</c>
+/// to reduce setup code.
+/// </summary>
+public sealed class TestDateTimeProvider : IDateTimeProvider
+{
+    private Func<DateTimeOffset> _dateTimeOffsetFactory;
+
+    public Func<DateTimeOffset> DateTimeOffsetFactory
+    {
+        get => _dateTimeOffsetFactory;
+        set
+        {
+            if (value().Offset != TimeSpan.Zero)
+                throw new ArgumentException("Factory must return time in UTC.", nameof(value));
+            _dateTimeOffsetFactory = value;
+        }
+    }
+
+    public TestDateTimeProvider(DateTimeOffset dateTimeOffset)
+    {
+        if (dateTimeOffset.Offset != TimeSpan.Zero)
+            throw new ArgumentException(
+                $"{nameof(dateTimeOffset)} must be in UTC.",
+                nameof(dateTimeOffset)
+            );
+
+        _dateTimeOffsetFactory = () => dateTimeOffset;
+    }
+
+    public TestDateTimeProvider(Func<DateTimeOffset> dateTimeOffsetFactory)
+    {
+        if (dateTimeOffsetFactory().Offset != TimeSpan.Zero)
+            throw new ArgumentException(
+                "Factory must return time in UTC.",
+                nameof(dateTimeOffsetFactory)
+            );
+
+        _dateTimeOffsetFactory = dateTimeOffsetFactory;
+    }
+
+    /// <inheritdoc />
+    public DateTimeOffset UtcNowOffset => DateTimeOffsetFactory();
 }
