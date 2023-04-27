077a8a181d10c5732bc852e6a5afa645fe33ac4f
Dmitry Zhavoronkov <rudomitori@gmail.com>
2023-04-27 12:16:32 +0700

    feat(DbRetryHelper): Add options to change isolation level

--- webapi/Lib/PersistenceHelpers/MccSoft.PersistenceHelpers/Extensions.cs
+++ webapi/Lib/PersistenceHelpers/MccSoft.PersistenceHelpers/Extensions.cs
@@ -1,11 +1,16 @@
+using System;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace MccSoft.PersistenceHelpers;
 
 public static class Extensions
 {
-    public static void RegisterRetryHelper(this IServiceCollection services)
+    public static void RegisterRetryHelper(
+        this IServiceCollection services,
+        Action<DbRetryHelperOptions>? setupAction = null
+    )
     {
+        services.Configure(setupAction ?? (_ => { }));
         services.AddScoped(typeof(TransactionLogger<>)).AddScoped(typeof(DbRetryHelper<,>));
     }
 }
