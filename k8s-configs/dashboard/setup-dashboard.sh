b70a9e5077289dc0b887cb2f14eee0f1ab174971
Artur Drobinskiy <artur.drobinskiy@mcc-tomsk.de>
2023-05-09 11:44:50 +0700

    fix k8s dashboard

--- k8s-configs/dashboard/setup-dashboard.sh
+++ k8s-configs/dashboard/setup-dashboard.sh
@@ -9,6 +9,6 @@ curl -sfL https://raw.githubusercontent.com/mccsoft/backend-frontend-template/ma
 kubectl apply -f dashboard/ingress.yaml
 kubectl apply -f dashboard/users.yaml
 #   generate token to login
-DASHBOARD_TOKEN = (kubectl -n kubernetes-dashboard get secret/admin-user-secret -o jsonpath="{.data.token}" | base64 --decode)
+DASHBOARD_TOKEN=$(kubectl -n kubernetes-dashboard get secret/admin-user-secret -o jsonpath="{.data.token}" | base64 --decode)
 echo $DASHBOARD_TOKEN > dashboard-token.txt
 echo "Your Kuberenetes Dashboard token (also in dashboard-token.txt): $DASHBOARD_TOKEN"
