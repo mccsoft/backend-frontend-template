c8eea4ab1f076f5c868ac984b6e22b541cc0360e
Artur Drobinskiy <artur.drobinskiy@mcc-tomsk.de>
2023-05-12 21:20:49 +0700

    Feature/ci kube (#101)
    * wip
    
    * fix signurl
    
    * wip
    
    * k3s: finish with initialization scripts

--- README.md
+++ README.md
@@ -87,8 +87,17 @@ Create a new git repository and copy everything (except `.git` folder) to it. Do
 1. Authenticate in your docker registry (if it's private). Run `docker login https://YOUR_DOCKER_REGISTRY`. Run `docker pull YOUR_FULL_IMAGE_URL` to make sure everything works.
 1. Run `curl -sfL https://raw.githubusercontent.com/mccsoft/backend-frontend-template/master/k8s-configs/setup.sh | /bin/bash -s -` from that folder. This will setup k3s, Kubernetes Dashboard (at `https://VIRTUAL_HOST/kube-dashboard`) and letsencrypt.
    1. Grab the contents of `dashboard-token.txt`, it contains the token you could use to login in Kubernetes Dashboard.
-1. Grab the `kubernetes-config`, change `localhost` to ip/DNS name of your VM, add it to Azure secret files with a name `kubernetes.config`.
+1. Grab the `cat /etc/rancher/k3s/k3s.yaml` from your VM and save it locally as `k3s.yaml`, change `server: https://localhost:6443` to `server: https://SERVER_IP_ADDRESS:6443` (it's important to put IP, not the Hostname there), add it to Azure secret files with a name `k3s.yaml`.
 1. Run your pipeline. Everything should be deployed.
+
+---
+
+### Reinitialize kubernetes
+
+1. If you want to change something in `.env` (e.g. **VIRTUAL_HOST**, **EMAIL**, or some other secret), change it in VM and run `curl -sfL https://raw.githubusercontent.com/mccsoft/backend-frontend-template/master/k8s-configs/reinit-namespace.sh | sh -s -`
+1. If you want to completely reinitialize whole kubernetes cluster, run the following:
+   1. `/usr/local/bin/k3s-uninstall.sh` to uninstall everything
+   1. `curl -sfL https://raw.githubusercontent.com/mccsoft/backend-frontend-template/master/k8s-configs/setup.sh | /bin/bash -s -` - to install everything (same script as you ran when set)
 
 ## Set up Hosting server (Droplet on Digital Ocean)
 
--- k8s-configs/setup.sh
+++ k8s-configs/setup.sh
@@ -6,13 +6,16 @@
 export $(cat ./.env | xargs)
 [ -z "$EMAIL" ] && (echo 'EMAIL env. variable is not defined' && exit 1)
 [ -z "$VIRTUAL_HOST" ] && (echo 'VIRTUAL_HOST env. variable is not defined (needed to configure DNS name)' && exit 1)
+[ -z "$NAMESPACE" ] && (echo 'NAMESPACE env. variable is not defined (needed to configure DNS name)' && exit 1)
 [ -z "$General__SiteUrl" ] && (echo "General__SiteUrl=$VIRTUAL_HOST" >> .env)
 
 # This file sets up k3s on fresh VPS:
 # 1. Install k3s
 # 2. Setup Let's encrypt
 # 3. Setup Kubernetes Dashboard
-# 4. Create Secret to authenticate in Docker Registry
+# 4. Create namespace
+# 5. Create Secret to authenticate in Docker Registry
+# 6. Create config maps (`$NAMESPACE-configmap-secret`)
 
 
 # 1. Install k3s 
@@ -39,23 +42,6 @@ kubectl apply -f letsencrypt.yaml
 # 3. Setup Kubernetes Dashboard
 curl -sfL https://raw.githubusercontent.com/mccsoft/backend-frontend-template/master/k8s-configs/dashboard/setup-dashboard.sh | sh -s -
 
-# 4. Create Secret to authenticate in Docker Registry
-kubectl delete secret docker-registry-secret
-test $HOME/.docker/config.json && kubectl create secret generic docker-registry-secret --from-file=.dockerconfigjson=$HOME/.docker/config.json --type=kubernetes.io/dockerconfigjson
 
-
-# kubectl create namespace templateapp
-
-# # 4. Setup Postgres
-# curl -sfL https://raw.githubusercontent.com/mccsoft/backend-frontend-template/master/k8s-configs/postgres.yaml > postgres.yaml
-# envsubst < postgres.yaml > postgres.yaml.tmp && mv postgres.yaml.tmp postgres.yaml
-# kubectl apply -f postgres.yaml
-
-# # 5. Setup App
-# # setup configmap
-# kubectl -n templateapp create configmap templateapp-main-configmap --from-env-file=.env || (kubectl -n templateapp create configmap templateapp-main-configmap --from-env-file=.env -o yaml --dry-run=client | kubectl replace -f -)
-# # setup deployment
-# curl -sfL https://raw.githubusercontent.com/mccsoft/backend-frontend-template/master/k8s-configs/templateapp-app.yaml > templateapp-app.yaml
-# envsubst < templateapp-app.yaml > templateapp-app.yaml.tmp && mv templateapp-app.yaml.tmp templateapp-app.yaml
-# kubectl apply -f templateapp-app.yaml
-
+# 4/5/6 creates and initializes namespace
+curl -sfL https://raw.githubusercontent.com/mccsoft/backend-frontend-template/master/k8s-configs/init-namespace.sh | sh -s -
