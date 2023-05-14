0db4de3a091d8f717db8c0071b4afdc81c121298
Artur Drobinskiy <artur.drobinskiy@mcc-tomsk.de>
2023-05-14 19:02:03 +0700

    CI: wip

--- k8s/init-namespace.sh
+++ k8s/init-namespace.sh
@@ -4,7 +4,7 @@ export $(cat ./.env | xargs)
 
 # This file sets up k3s on fresh VPS:
 # 1. Create namespace
-kubectl create namespace $NAMESPACE
+kubectl create namespace $NAMESPACE || echo "$NAMESPACE exists"
 
 # 2. Create secret config maps (`$NAMESPACE-configmap-secret`)
 kubectl -n $NAMESPACE create configmap $NAMESPACE-configmap-secret --from-env-file=.env || (kubectl -n $NAMESPACE create configmap $NAMESPACE-configmap-secret --from-env-file=.env -o yaml --dry-run=client | kubectl replace -f -)
