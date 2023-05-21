a17c34be983ee55bdcf783382236f048acb1b59a
Artur Drobinskiy <artur.drobinskiy@mcc-tomsk.de>
2023-05-21 13:15:41 +0700

    SCRIPTS: fix

--- scripts/pull-template-changes.js
+++ scripts/pull-template-changes.js
@@ -2,7 +2,7 @@
 import { execSync } from 'child_process';
 import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url);
-execSync(`npx ts-node ${__filename.replace('.js', '.ts')}`, {
+execSync(`npx ts-node --esm ${__filename.replace('.js', '.ts')}`, {
   cwd: __filename.replace('pull-template-changes.js', '..'),
   stdio: 'inherit',
 });
