c9372b49deb3a7b98bff032fc3225ed8af809913
Artur Drobinskiy <artur.drobinskiy@mcc-tomsk.de>
2023-05-28 11:08:27 +0700

    apply autoprefix

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig, loadEnv } from 'vite';
+import autoprefixer from 'autoprefixer';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import react from '@vitejs/plugin-react';
 import reactSwc from '@vitejs/plugin-react-swc';
@@ -74,6 +75,9 @@ export default defineConfig(({ command, mode }) => {
     css: {
       modules: {
         localsConvention: 'camelCase',
+      },
+      postcss: {
+        plugins: [autoprefixer],
       },
     },
     build: {
