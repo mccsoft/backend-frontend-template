b6a1cb43bce59be4b664f04f2d0eabe4a9d167de
Artur Drobinskiy <artur.drobinskiy@mcc-tomsk.de>
2023-06-22 21:47:40 +0700

    update i18next

--- frontend/src/application/localization/useScopedTranslation.ts
+++ frontend/src/application/localization/useScopedTranslation.ts
@@ -1,21 +1,23 @@
 import {
+  FallbackNs,
   useTranslation,
   UseTranslationOptions,
   UseTranslationResponse,
 } from 'react-i18next';
-import { KeyPrefix, Namespace } from 'i18next';
+import { FlatNamespace, KeyPrefix } from 'i18next';
+import { $Tuple } from 'react-i18next/helpers';
 /*
 Allows to specify the initial path of all translations returned by the function.
 E.g. useScopedTranslation('Page.Login'). All translations will start with 'Page.Login.*'
  */
 export function useScopedTranslation<
-  N extends Namespace,
-  TKPrefix extends KeyPrefix<N>,
+  Ns extends FlatNamespace | $Tuple<FlatNamespace> | undefined = undefined,
+  TKPrefix extends KeyPrefix<FallbackNs<Ns>> = undefined,
 >(
   path: TKPrefix,
-  ns?: N | Readonly<N>,
+  ns?: Ns,
   options?: UseTranslationOptions<TKPrefix>,
-): UseTranslationResponse<N, TKPrefix> {
+): UseTranslationResponse<FallbackNs<Ns>, TKPrefix> {
   const i18n = useTranslation(ns, { ...options, keyPrefix: path });
   return i18n;
 }
