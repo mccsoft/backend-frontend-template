a1db53bda8af304d7bfd167183e1bfc2a08fee1e
Artur Drobinskiy <artur.drobinskiy@mcc-soft.de>
2024-05-15 12:34:59 +0500

    DateTimeHelper with static way to get DateTime.UtcNow, but be able to override that in tests

---
+++ webapi/Lib/LowLevelPrimitives/MccSoft.LowLevelPrimitives/DateTimeHelper.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading;
+
+namespace MccSoft.LowLevelPrimitives;
+
+/// <summary>
+/// Helper to be able to override DateTime.UtcNow in tests, but avoid using/injecting IDateTimeProvider everywhere
+/// (especially in Domain entities)
+/// </summary>
+public static class DateTimeHelper
+{
+    private static readonly AsyncLocal<DateTime?> _customDateTime = new();
+    public static DateTime UtcNow => _customDateTime.Value ?? DateTime.UtcNow;
+
+    public static Disposable SetCustomDateTime(DateTime dateTime)
+    {
+        var oldValue = _customDateTime.Value;
+        _customDateTime.Value = dateTime;
+        return new Disposable(() =>
+        {
+            _customDateTime.Value = oldValue;
+        });
+    }
+}
