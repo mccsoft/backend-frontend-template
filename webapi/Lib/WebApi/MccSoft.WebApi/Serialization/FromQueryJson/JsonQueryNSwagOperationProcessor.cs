da446e186d4f92d21fa74210c0aa60a6486b46fc
Artur Drobinskiy <artur.drobinskiy@mcc-soft.de>
2024-05-27 23:03:06 +0500

    fix [FromJsonQuery] support for nested attributes

--- webapi/Lib/WebApi/MccSoft.WebApi/Serialization/FromQueryJson/JsonQueryNSwagOperationProcessor.cs
+++ webapi/Lib/WebApi/MccSoft.WebApi/Serialization/FromQueryJson/JsonQueryNSwagOperationProcessor.cs
@@ -19,31 +19,38 @@ public class JsonQueryNSwagOperationProcessor : IOperationProcessor
 {
     public bool Process(OperationProcessorContext context)
     {
-        if (context is not AspNetCoreOperationProcessorContext aspNetCoreOperationProcessorContext)
-            return true;
-
-        var jsonQueryParamNames =
-            aspNetCoreOperationProcessorContext.ApiDescription.ParameterDescriptions
-                .Where(x => x.BindingInfo?.BinderType == typeof(JsonQueryBinder))
-                .Select(x => x.Name);
-
-        foreach (var jsonQueryParamName in jsonQueryParamNames)
+        if (context is AspNetCoreOperationProcessorContext aspNetOperationContext)
         {
-            var openApiParameter =
-                aspNetCoreOperationProcessorContext.OperationDescription.Operation.Parameters.First(
-                    x => x.Name == jsonQueryParamName
-                );
-
-            var content = new Dictionary<string, OpenApiMediaType>
+            var parameterDescriptions = aspNetOperationContext.ApiDescription.ParameterDescriptions;
+            var parameters = aspNetOperationContext.OperationDescription.Operation.Parameters;
+            for (int i = 0; i < parameterDescriptions.Count; i++)
             {
-                [MediaTypeNames.Application.Json] = new() { Schema = openApiParameter.Schema, }
-            };
-            openApiParameter.ExtensionData ??= new Dictionary<string, object>();
-            openApiParameter.ExtensionData.Add("content", content);
-            // We need to add schema somewhere where it could be parsed by
-            // JsonSchemaReferenceUtilities.UpdateSchemaReferencePaths.
-            openApiParameter.AdditionalItemsSchema = openApiParameter.Schema;
-            openApiParameter.Schema = null;
+                var parameterDescription = parameterDescriptions[i];
+                if (parameterDescription.ModelMetadata is DefaultModelMetadata defaultModelMetadata)
+                {
+                    if (
+                        defaultModelMetadata.Attributes.Attributes.Any(
+                            attribute => attribute.GetType() == typeof(FromJsonQueryAttribute)
+                        )
+                    )
+                    {
+                        var openApiParameter = parameters[i];
+                        var content = new Dictionary<string, OpenApiMediaType>()
+                        {
+                            [MediaTypeNames.Application.Json] = new OpenApiMediaType()
+                            {
+                                Schema = openApiParameter.Schema,
+                            }
+                        };
+                        openApiParameter.ExtensionData ??= new Dictionary<string, object>();
+                        openApiParameter.ExtensionData.Add("content", content);
+                        // We need to add schema somewhere where it could be parsed by
+                        // JsonSchemaReferenceUtilities.UpdateSchemaReferencePaths.
+                        openApiParameter.AdditionalItemsSchema = openApiParameter.Schema;
+                        openApiParameter.Schema = null;
+                    }
+                }
+            }
         }
 
         return true;
