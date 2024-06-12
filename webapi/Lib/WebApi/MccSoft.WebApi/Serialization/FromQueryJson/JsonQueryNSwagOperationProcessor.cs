0fb8545380e4c4aa523d3a3decfc346a8b355368
Artur Drobinskiy <artur.drobinskiy@mcc-soft.de>
2024-06-12 10:24:27 +0500

    fix build

--- webapi/Lib/WebApi/MccSoft.WebApi/Serialization/FromQueryJson/JsonQueryNSwagOperationProcessor.cs
+++ webapi/Lib/WebApi/MccSoft.WebApi/Serialization/FromQueryJson/JsonQueryNSwagOperationProcessor.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
 using System.Net.Mime;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Metadata;
 using NSwag;
 using NSwag.Generation.AspNetCore;
 using NSwag.Generation.Processors;
