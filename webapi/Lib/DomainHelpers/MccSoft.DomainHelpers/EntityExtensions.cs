4a1be7418bd88b90f5f5323f0e8300f102dd927a
Maxim Vasin <maxim.vasin@mcc-soft.de>
2025-04-23 21:16:56 +0700

    refactor(lib): add ThrowIfNotIncluded domain helper

---
+++ webapi/Lib/DomainHelpers/MccSoft.DomainHelpers/EntityExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+using System.Runtime.CompilerServices;
+
+namespace MccSoft.DomainHelpers;
+
+public static class EfCoreExtensions
+{
+    // https://learn.microsoft.com/en-us/ef/core/miscellaneous/nullable-reference-types#required-navigation-properties
+    [StackTraceHidden]
+    public static T ThrowIfNotIncluded<T>(
+        [NotNull] this T? backingField,
+        [CallerMemberName] string navigationPropName = ""
+    )
+        where T : class
+    {
+        return backingField
+            ?? throw new NullReferenceException($"{navigationPropName} was not included");
+    }
+}
