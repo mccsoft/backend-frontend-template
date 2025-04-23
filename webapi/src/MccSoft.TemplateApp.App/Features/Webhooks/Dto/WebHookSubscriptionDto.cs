3fdaf97dc6ea8a582a6f2615d8a908bd948174c5
Maxim Vasin <maxim.vasin@mcc-soft.de>
2025-04-23 22:39:24 +0700

    feat(webhooks): add subscribe controller

---
+++ webapi/src/MccSoft.TemplateApp.App/Features/Webhooks/Dto/WebHookSubscriptionDto.cs
@@ -0,0 +1,16 @@
+﻿namespace MccSoft.TemplateApp.App.Features.Webhooks.Dto;
+
+/// <summary>
+/// Webhook subscription DTO
+/// </summary>
+/// <para name="Id">Subscription Id</para>
+/// <param name="Name">Human-readable name of integration</param>
+/// <param name="Url">URL for integration</param>
+public record WebhookSubscriptionDto(
+    Guid Id,
+    string Name,
+    string Url,
+    WebHookEventType eventType,
+    string? method = null,
+    Dictionary<string, string>? headers = null
+);
