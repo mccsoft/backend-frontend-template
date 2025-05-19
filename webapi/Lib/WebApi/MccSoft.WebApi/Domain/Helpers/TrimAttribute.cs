701996ee3dc4047b5a7a06ecc5c18feda5545574
Artur Drobinskiy <artur.drobinskiy@mcc-soft.de>
2025-05-19 10:42:58 +0500

    Trim attribute

---
+++ webapi/Lib/WebApi/MccSoft.WebApi/Domain/Helpers/TrimAttribute.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MccSoft.WebApi.Domain.Helpers;
+
+/// <summary>
+/// Trims the string that is passed from the client
+/// </summary>
+public class TrimAttribute : ValidationAttribute
+{
+    protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+    {
+        if (value is not string stringValue)
+            return ValidationResult.Success;
+        if (string.IsNullOrEmpty(stringValue))
+            return ValidationResult.Success;
+        var trimmedValue = stringValue.Trim();
+        if (trimmedValue == stringValue)
+            return ValidationResult.Success;
+
+        var parent = validationContext.ObjectInstance;
+        var type = parent.GetType();
+        type.GetProperty(validationContext.MemberName!)!.SetValue(parent, trimmedValue);
+        return ValidationResult.Success;
+    }
+}
--- webapi/src/MccSoft.TemplateApp.App/Features/Products/Dto/CreateProductDto.cs
+++ webapi/src/MccSoft.TemplateApp.App/Features/Products/Dto/CreateProductDto.cs
@@ -1,11 +1,13 @@
 using System.ComponentModel.DataAnnotations;
 using MccSoft.TemplateApp.Domain;
+using MccSoft.WebApi.Domain.Helpers;
 
 namespace MccSoft.TemplateApp.App.Features.Products.Dto;
 
 public class CreateProductDto
 {
     [Required]
+    [Trim]
     [MinLength(3)]
     public string Title { get; set; }
 
