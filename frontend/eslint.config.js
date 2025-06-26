8e27ab2d62462a6a20043de4e891bd24a1bcd7d7
Artur Drobinskiy <artur.drobinskiy@mcc-soft.de>
2025-06-26 15:05:30 +0500

    update eslint

---
+++ frontend/eslint.config.js
@@ -0,0 +1,86 @@
+const { defineConfig, globalIgnores } = require('eslint/config');
+
+const tsParser = require('@typescript-eslint/parser');
+
+const { fixupConfigRules } = require('@eslint/compat');
+
+const js = require('@eslint/js');
+
+const { FlatCompat } = require('@eslint/eslintrc');
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
+});
+
+module.exports = defineConfig([
+  {
+    languageOptions: {
+      parser: tsParser,
+      ecmaVersion: 2020,
+      sourceType: 'module',
+
+      parserOptions: {
+        project: 'tsconfig.json',
+
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
+
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+
+    extends: fixupConfigRules(
+      compat.extends(
+        'plugin:@typescript-eslint/recommended',
+        'plugin:react-hooks/recommended',
+        'prettier',
+        'plugin:storybook/recommended',
+      ),
+    ),
+
+    rules: {
+      'react/prop-types': 'off',
+      '@typescript-eslint/no-var-requires': 'off',
+      '@typescript-eslint/no-use-before-define': 'off',
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      '@typescript-eslint/ban-ts-ignore': 'off',
+      'react/display-name': 'off',
+      'jsx-a11y/anchor-is-valid': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-non-null-assertion': 'off',
+      '@typescript-eslint/ban-ts-comment': 'off',
+      '@typescript-eslint/no-empty-function': 'warn',
+      '@typescript-eslint/ban-types': 'warn',
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
+
+      '@typescript-eslint/no-unused-vars': [
+        'warn',
+        {
+          argsIgnorePattern: '^_',
+        },
+      ],
+
+      '@typescript-eslint/no-inferrable-types': 'off',
+      '@typescript-eslint/no-floating-promises': 'error',
+      '@typescript-eslint/await-thenable': 'error',
+    },
+  },
+  globalIgnores([
+    '**/node_modules',
+    '**/package.json',
+    '**/package.json',
+    '**/build/**/*',
+    '**/.idea',
+    '**/.vscode',
+    '**/vite.config.ts',
+    'scripts/**/*',
+    '.storybook/main.ts',
+  ]),
+]);
