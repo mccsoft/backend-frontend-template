7dc27f408f009e2cdf940dbd2a500863d4446ccc
Artur Drobinskiy <artur.drobinskiy@mcc-soft.de>
2025-06-27 14:37:18 +0500

    fix isDirty

--- frontend/src/pages/authorized/products/create/CreateProductPage.tsx
+++ frontend/src/pages/authorized/products/create/CreateProductPage.tsx
@@ -32,11 +32,11 @@ export const CreateProductPage: React.FC = () => {
   });
   const modals = useModal();
 
-  // we need to access `isDirty` during rendering, otherwise the value inside useBlockNavigation won't be updated
-  const isDirty = form.formState.isDirty;
-  console.log('qweqwe', form.formState.dirtyFields);
+  // we need to access `dirtyFields` during rendering, otherwise the value inside useBlockNavigation won't be updated
+  // we need to use `.dirtyFields` instead of `.isDirty` because latter only works if we set `defaultValues`
+  const dirtyFields = form.formState.dirtyFields;
   useBlockNavigation(async () => {
-    if (!isDirty) return false;
+    if (Object.keys(dirtyFields).length === 0) return false;
     const confirmResult = await modals.showConfirm({
       title: i18n.t('unsaved_changes_prompt.title'),
       text: i18n.t('unsaved_changes_prompt.text'),
