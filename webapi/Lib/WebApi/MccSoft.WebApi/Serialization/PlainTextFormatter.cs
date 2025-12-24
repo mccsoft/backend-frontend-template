5e688cd7c7a864c6749c2de40ac77006e3868de5
Artur Drobinskiy <artur.drobinskiy@mcc-soft.de>
2025-12-24 08:10:53 +0500

    PlainTextFormatter

---
+++ webapi/Lib/WebApi/MccSoft.WebApi/Serialization/PlainTextFormatter.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+using System.Net.Mime;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Formatters;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Net.Http.Headers;
+
+namespace MccSoft.WebApi.Serialization;
+
+public class PlainTextFormatter : TextInputFormatter
+{
+    public PlainTextFormatter()
+    {
+        SupportedMediaTypes.Add(new MediaTypeHeaderValue(MediaTypeNames.Text.Plain));
+
+        SupportedEncodings.Add(Encoding.UTF8);
+        SupportedEncodings.Add(Encoding.ASCII);
+    }
+
+    public override async Task<InputFormatterResult> ReadRequestBodyAsync(
+        InputFormatterContext context,
+        Encoding encoding
+    )
+    {
+        using var reader = new StreamReader(context.HttpContext.Request.Body, encoding);
+        var plainText = await reader.ReadToEndAsync();
+
+        return await InputFormatterResult.SuccessAsync(plainText);
+    }
+
+    protected override bool CanReadType(Type type) => type == typeof(string);
+}
+
+public static class PlainTextFormatterExtensions
+{
+    public static IMvcBuilder AddPlainTextFormatter(this IServiceCollection serviceCollection)
+    {
+        return serviceCollection.AddControllers(options =>
+        {
+            options.InputFormatters.Insert(0, new PlainTextFormatter());
+        });
+    }
+}
